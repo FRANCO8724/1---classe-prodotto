0c3f382d5b1d0638098faaaaf38571c0cbc28969
FRANCOHAILIMITIDELLEVOLUTO <121171151+FRANCO8724@users.noreply.github.com>
2023-10-02 22:04:17 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    class Prodotto
+    {
+        //Attributi
+        private string nome;
+        private float prezzo;
+
+        //Costruttore con parametri
+        public Prodotto(string mnome, float mprezzo)
+        {
+            nome = mnome;
+            prezzo = mprezzo;
+        }
+        
+        //Costruttore senza parametri
+        public Prodotto()
+        {
+            nome = "pesca";
+            prezzo = 4;
+        }
+
+        //Metodo
+        public string Elenco()
+        {
+            return nome + " " + Convert.ToString(prezzo);
+        }
+
+    }
+
+    static void Main()
+    {
+        Prodotto prodotto1 = new Prodotto("Mele",3);
+        Prodotto prodotto2 = new Prodotto("Pesca", 4);
+
+        Console.WriteLine(prodotto1.Elenco());
+        Console.WriteLine(prodotto2.Elenco());
+    }
+
+    
+
+}
